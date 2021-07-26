585f5c5050718613d8ac76cc600c8211d2c7661e
Akim Malyschik <akimich11@yandex.by>
2021-07-26 16:17:54 +0300

    init commit

---
+++ coursera_house/core/tasks.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+from celery import task
+
+from .models import Setting
+
+
+@task()
+def smart_home_manager():
+    # Здесь ваш код для проверки условий
+    pass
