881a536ab07ca2905dd4b9ae7c4cb3cb53536eb4
Akim Malyschik <akimich11@yandex.by>
2021-07-27 16:35:23 +0300

    all done, not tested

--- coursera_house/core/views.py
+++ coursera_house/core/views.py
@@ -1,3 +1,7 @@
+import json
+import os
+import tempfile
+
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
@@ -12,11 +16,33 @@ class ControllerView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(ControllerView, self).get_context_data()
-        context['data'] = {}
+        try:
+            with open(os.path.join(tempfile.gettempdir(), 'controller.json')) as f:
+                context['data'] = json.load(f)  # Current values
+        except FileNotFoundError:
+            context['data'] = {}
         return context
 
     def get_initial(self):
-        return {}
+        try:
+            with open(os.path.join(tempfile.gettempdir(), 'controller.json')) as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            data = {}
+        return {
+            'bedroom_light': data['bedroom_light'],
+            'bathroom_light': data['bathroom_light'],
+            'hot_water_target_temperature': Setting.objects.get(controller_name='hot_water_target_temperature').value,
+            'bedroom_target_temperature': Setting.objects.get(controller_name='bedroom_target_temperature').value
+        }
 
-    def form_valid(self, form):
-        return super(ControllerView, self).form_valid(form)
+
+def form_valid(self, form):
+    with open(os.path.join(tempfile.gettempdir(), 'form.json'), 'w') as f:
+        json.dump({'bedroom_light': form.cleaned_data['bedroom_light'],
+                   'bathroom_light': form.cleaned_data['bathroom_light']}, f)
+    Setting.objects.filter(controller_name='hot_water_target_temperature').update(
+        value=form.cleaned_data['hot_water_target_temperature'])
+    Setting.objects.filter(controller_name='bedroom_target_temperature').update(
+        value=form.cleaned_data['bedroom_target_temperature'])
+    return super(ControllerView, self).form_valid(form)
